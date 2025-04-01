61a52f623170158bfc5abd4394f34866ab403594
geegpay95 <geegpay959@gmail.com>
2025-04-01 03:08:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "bluecoralbeachresort7@gmail.com";
-const pass = "";
+const pass = "xquekbdhhxggmzjz";
 // 15 APRIL
 
 // Middleware
